f9288bd25390dba8e2e14a99d5b1a8ee70a92d6b
none <alexander.podkidyshev@gmail.com>
2012-05-11 12:24:37 +0300

    Has modimied routes.rb and added root route, and deleted public/index.html

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ TestUploadify::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pictures#index'
 
   # See how all your routes lay out with "rake routes"
 
